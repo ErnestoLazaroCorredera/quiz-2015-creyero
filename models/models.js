5ff9f961fe681c86470711293e245be9fe18e2c7
Carlos Reyero <c.reyero@gmail.com>
2015-07-15 21:13:31 +0200

    M07 - Lista Preguntas

--- models/models.js
+++ models/models.js
@@ -29,14 +29,17 @@ var sequelize = new Sequelize(DB_name,user,pwd,
 var Quiz = sequelize.import(path.join (__dirname, 'quiz'));
 exports.Quiz = Quiz; // exportar definicion de tabla Quiz
 // sequelize.sync() crea e inicializa la tabla de preguntas en DB
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
 // success(..) ejecuta el manejador una vez creada la tabla
-Quiz.count().success(function (count){
-  if (count==0) { //la tabla se inicializa solo si esta vacia
+Quiz.count().then(function (count){
+  if (count===0) { //la tabla se inicializa solo si esta vacia
      Quiz.create({ pregunta: 'Capital de Italia',
                    respuesta: 'Roma'
-                 })
-      .success(function(){console.log('Base de datos inicializada')});
+                 });
+     Quiz.create({ pregunta: 'Capital de Portugal',
+                   respuesta: 'Lisboa'
+                             })
+      .then(function(){console.log('Base de datos inicializada')});
     };
 });
 });
