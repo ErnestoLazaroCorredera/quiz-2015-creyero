7c1445630d8b45c409ff55add91b008378bf9ae6
Carlos Reyero <c.reyero@gmail.com>
2015-07-19 17:38:58 +0200

    M07 - Autoload

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,19 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+res.render('index', { title: 'Quiz' });
 });
+
+//Autoload de comandos con :quizId
+router.param('quizId',quizController.load); //autoload :quizId
 
 // Definicion de rutas de /quizes
 router.get('/quizes',                  quizController.index);
 router.get('/quizes/:quizId(\\d+)',    quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer', quizController.answer);
 router.get('/author', quizController.author);
+//router.get('/quizes/question',quizController.question);
+//router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
